cb1d3dd132b58931b2722d3ae8d1f432e5a3b6f6
xdd <45672942@qq.com>
2024-10-20 17:26:34 +0800

    fix sidebar links to absolute paths.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,6 +9,7 @@ const RSSOptions: RSSOptions = {
 export default {
   title: 'Blog',
   lastUpdated: true,
+  base: '/',
   vite: {
     plugins: [RssPlugin(RSSOptions)],
   },
@@ -55,80 +56,80 @@ function sidebar() {
     {
       text: '2024TODO',
       items: [
-        { text: '2024TODO', link: './posts/2024-todo/todo' },
+        { text: '2024TODO', link: '/posts/2024-todo/todo' },
       ]
     },
     {
       text: '踩坑日记',
       items: [
-        { text: 'webpack打包', link: './posts/踩坑日记/webpack打包' },
+        { text: 'webpack打包', link: '/posts/踩坑日记/webpack打包' },
       ]
     },
     {
       text: 'Notes',
       items: [
-        { text: 'React', link: './posts/notes/React' },
-        { text: 'TS', link: './posts/notes/ts' },
-        { text: 'JS', link: './posts/notes/js' },
-        { text: 'Array', link: './posts/notes/array' },
-        { text: 'Npm ', link: './posts/notes/npm' },
-        { text: 'How to Learn', link: './posts/notes/如何快速学习新技术' },
-        { text: 'Git提交规范', link: './posts/notes/git提交规范' },
-        { text: '切换IDE的终端为linux', link: './posts/notes/在windows中切换IDE的终端为linux.md' },
-        { text: 'gitignore过滤规则', link: './posts/notes/gitignore的作用.md' },
+        { text: 'React', link: '/posts/notes/React' },
+        { text: 'TS', link: '/posts/notes/ts' },
+        { text: 'JS', link: '/posts/notes/js' },
+        { text: 'Array', link: '/posts/notes/array' },
+        { text: 'Npm ', link: '/posts/notes/npm' },
+        { text: 'How to Learn', link: '/posts/notes/如何快速学习新技术' },
+        { text: 'Git提交规范', link: '/posts/notes/git提交规范' },
+        { text: '切换IDE的终端为linux', link: '/posts/notes/在windows中切换IDE的终端为linux' },
+        { text: 'gitignore过滤规则', link: '/posts/notes/gitignore的作用' },
       ]
     },
     {
       text: 'JavaScript',
       items: [
-        { text: 'import和export', link: './posts/js/import' }
+        { text: 'import和export', link: '/posts/js/import' }
       ]
     },
     {
       text: 'TypeScript',
       items: [
-        { text: 'TypeScript总结', link: './posts/ts/index' },
+        { text: 'TypeScript总结', link: '/posts/ts/index' },
       ]
     },
     {
       text: 'nodeJs学习',
       items: [
-        { text: 'Es Module和commonJs', link: './posts/nodeJs/esm和commonJs' },
-        { text: '全局变量&全局API', link: './posts/nodeJs/全局变量和全局API' },
-        { text: 'path方法', link: './posts/nodeJs/path' },
-        { text: 'os模块', link: './posts/nodeJs/os' },
-        { text: 'exec小实践', link: './posts/nodeJs/exec' },
-        { text: '图片裁剪', link: './posts/nodeJs/图片裁剪' },
+        { text: 'Es Module和commonJs', link: '/posts/nodeJs/esm和commonJs' },
+        { text: '全局变量&全局API', link: '/posts/nodeJs/全局变量和全局API' },
+        { text: 'path方法', link: '/posts/nodeJs/path' },
+        { text: 'os模块', link: '/posts/nodeJs/os' },
+        { text: 'exec小实践', link: '/posts/nodeJs/exec' },
+        { text: '图片裁剪', link: '/posts/nodeJs/图片裁剪' },
       ]
     },
     {
       text: 'Linux',
       items: [
-        { text: 'Linux常用命令', link: './posts/linux/linux常用命令' },
+        { text: 'Linux常用命令', link: '/posts/linux/linux常用命令' },
       ]
     },
     {
       text: '前端工程化实践',
       items: [
-        { text: 'package.json文件配置', link: './posts/前端工程化实践/packjson文件配置' },
+        { text: 'package.json文件配置', link: '/posts/前端工程化实践/packjson文件配置' },
       ]
     },
     {
       text: 'Nginx',
       items: [
-        { text: 'Nginx学习', link: './posts/nginx/index' },
+        { text: 'Nginx学习', link: '/posts/nginx/index' },
       ]
     },
     {
       text: 'WorkFlow',
       items: [
-        { text: 'Github Action', link: './posts/workflow/index' }
+        { text: 'Github Action', link: '/posts/workflow/index' }
       ]
     },
     {
       text: 'Markdown',
       items: [
-        { text: 'Md Notes', link: './posts/learn/md' },
+        { text: 'Md Notes', link: '/posts/learn/md' },
       ]
     },
   ]
